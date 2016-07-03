9e501b7cd58463cb6194ea434135983c237c400c
Afram <marwan.butrous@protonmail.ch>
2016-07-03 20:34:25 +0100

    update ts typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,14 +1,14 @@
+import * as React from "react";
+
 declare module "react-masonry-component" {
-    import React = __React;
 
     interface MasonryPropTypes {
         disableImagesLoaded?: boolean;
         updateOnEachImageLoad?: boolean;
-        onImagesLoaded?: Function;
+        onImagesLoaded?: (instance: any) => void;
         options?: Object;
         className?: string;
         elementType: string;
-
     }
 
     export var Masonry: React.Component<MasonryPropTypes, void>;
