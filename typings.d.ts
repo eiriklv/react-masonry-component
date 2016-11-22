f9a2e32e2075c6998af10b0550126c3d17b2efb7
cwmoo740 <cwmoo740@gmail.com>
2016-11-22 14:27:14 -0500

    typings.d.ts using export as namespace syntax as recommended for external modules

--- typings.d.ts
+++ typings.d.ts
@@ -1,32 +1,30 @@
 import { ComponentClass } from "react";
 
-declare module "react-masonry-component" {
-
-    export interface MasonryOptions {
-        columnWidth?: number;
-        itemSelector?: string;
-        gutter?: number;
-        percentPosition?: boolean;
-        stamp?: string;
-        fitWidth?: boolean;
-        originLeft?: boolean;
-        originTop?: boolean;
-        containerStyle?: Object;
-        transitionDuration?: string;
-        resize?: boolean;
-        initLayout?: boolean;
-    }
-
-    interface MasonryPropTypes {
-        disableImagesLoaded?: boolean;
-        updateOnEachImageLoad?: boolean;
-        onImagesLoaded?: (instance: any) => void;
-        options?: MasonryOptions;
-        className?: string;
-        elementType?: string;
-        style?: Object;
-    }
-
-    const Masonry: ComponentClass<MasonryPropTypes>;
-    export default Masonry;
+export interface MasonryOptions {
+    columnWidth?: number;
+    itemSelector?: string;
+    gutter?: number;
+    percentPosition?: boolean;
+    stamp?: string;
+    fitWidth?: boolean;
+    originLeft?: boolean;
+    originTop?: boolean;
+    containerStyle?: Object;
+    transitionDuration?: string;
+    resize?: boolean;
+    initLayout?: boolean;
 }
+
+export interface MasonryPropTypes {
+    disableImagesLoaded?: boolean;
+    updateOnEachImageLoad?: boolean;
+    onImagesLoaded?: (instance: any) => void;
+    options?: MasonryOptions;
+    className?: string;
+    elementType?: string;
+    style?: Object;
+}
+
+declare const Masonry: ComponentClass<MasonryPropTypes>;
+export as namespace Masonry;
+export default Masonry;
